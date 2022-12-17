939fb09e91fe2c4f40042f8f62fda1e9a3779ec9
Emil Sharkov <emosharkov@gmail.com>
2022-12-16 16:47:48 -0800

    version 1.0 of cli soundcloud downloader

--- soundcloud2spotify.js
+++ soundcloud2spotify.js
@@ -1,31 +1,39 @@
 const SoundCloud = require("soundcloud-scraper");
-var exec = require('child_process').exec;
 const client = new SoundCloud.Client();
 const fs = require("fs");
 
-// if(process.argv.length != 4){
-//     die("Usage: node soundcloud2spotify.js <PATH> <SONG URL>")
-// }
+const main = async => {
+    var downloadFolder = process.env.USERPROFILE + '\\Downloads';
+    if(process.argv.length != 3 && process.argv.length != 4){
+        console.log("Usage: node soundcloud2spotify.js <SONG URL> (<new name>)")
+        process.exit()
+    }
 
-let url = "https://soundcloud.com/benzvibez/playboi-carti-rockstar-made-remix-slowedreverb-prod-benz-music"
-let sanitizedURL = url.split("?")
-console.log(sanitizedURL)
-
-download(sanitizedURL[0])
-
-
-async function download (url) {
-    console.log("penis")
-
-    let songDownload = await client.getSongInfo(url)
-    .then(async song => {
-        const stream = await song.downloadProgressive();
-        const writer = stream.pipe(fs.createWriteStream(`./pp.mp3`));
-        writer.on("finish", () => {
-        console.log("Finished writing song!")
-        });
-    })
-    .catch(console.error);
-
-    console.log("benis")
+    let url = process.argv[2]
+    let parsedUrl = url.split("?")
+    console.log(url)
+    downloadSong(parsedUrl[0],downloadFolder)
 }
+
+const downloadFile = async (song, downloadsDirectory) => {
+    process.chdir(downloadsDirectory)
+    let songName = process.argv.length === 3 ? song.title.replace(/[\\/:*?\"<>|]/g, ""): process.argv[3]
+
+    return new Promise(async resolve => {
+        const stream = await song.downloadProgressive();
+        const writer = stream.pipe(fs.createWriteStream(`./${songName}.mp3`));
+        writer.on("finish", () => {
+            console.log("Finished writing song!")
+            resolve()
+        })
+    })
+}
+
+const downloadSong = async (url,downloadsDirectory) => {
+    console.log("Starting Download")
+    let song = await client.getSongInfo(url)
+    await downloadFile(song,downloadsDirectory)
+    console.log(`You may know find this song in ${downloadsDirectory}`)
+}
+
+main()
